051ab65289042fc7d63df7a4feb2150db255054d
linlichieh <jack.lin.293@gmail.com>
2023-02-23 20:54:22 +0800

    Refactor leetcode 21 and improve memory use

--- leetcode/Linked-List/21_Merge_Two_Sorted_Lists/leetcode21.go
+++ leetcode/Linked-List/21_Merge_Two_Sorted_Lists/leetcode21.go
@@ -9,18 +9,21 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	if list1 == nil && list2 == nil {
 		return nil
 	}
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
 
 	var min *ListNode
-	if list1 != nil && (list2 == nil || list1.Val < list2.Val) {
+	if list1.Val < list2.Val {
 		min = list1
 		list1 = list1.Next
 	} else {
 		min = list2
 		list2 = list2.Next
 	}
-
-	return &ListNode{
-		Val:  min.Val,
-		Next: mergeTwoLists(list1, list2),
-	}
+	min.Next = mergeTwoLists(list1, list2)
+	return min
 }
