5afd8e2e016984569443f07a7a6e5549e72464ff
linlichieh <jack.lin.293@gmail.com>
2023-05-13 19:43:09 +0800

    Improve the performance of the bubble sort when sorting 100,000 sorted numbers

--- algorithms/bubble_sort/bubble.go
+++ algorithms/bubble_sort/bubble.go
@@ -2,10 +2,16 @@ package bubbleSort
 
 func bubble(arr []int) []int {
 	for i := len(arr) - 1; i > 0; i-- {
+		swapped := false
 		for j := 0; j < i; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
 			}
+		}
+		// If no two elements were swapped by inner loop, it indicates the numbers are sorted
+		if !swapped {
+			break
 		}
 	}
 	return arr
